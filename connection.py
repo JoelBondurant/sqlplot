1e17aaf20b8389e18a77e4820b7747444748576e
Joel Bondurant <joel@analyticobjects.com>
2021-06-22 06:46:27 -0700

    Editable connections.

--- connection.py
+++ connection.py
@@ -24,23 +24,40 @@ def is_valid(form):
 	return True
 
 
-@aiohttp_jinja2.template('html/connection.html')
 async def connection(request):
 	async with (request.app['pg_pool']).acquire(timeout=2) as pgconn:
 		columns = ['xid'] + FORM_FIELDS.copy()
 		if request.method == 'POST':
 			form = await request.post()
 			if is_valid(form):
-				xid = 'x' + secrets.token_hex(16)[1:]
-				record = tuple([xid] + [form[k] for k in FORM_FIELDS])
-				logging.debug(f'Record: {record}')
-				result = await pgconn.copy_records_to_table('connection', records=[record], columns=columns)
+				if len(form['xid']) == 32:
+					xid = form['xid']
+					record = tuple([xid] + [form[k] for k in FORM_FIELDS])
+					await pgconn.execute('''
+						update connection
+						set name = $2, configuration = $3
+						where xid = $1;
+					''', xid, form['name'], form['configuration'])
+				else:
+					xid = 'x' + secrets.token_hex(16)[1:]
+					record = tuple([xid] + [form[k] for k in FORM_FIELDS])
+					logging.debug(f'Record: {record}')
+					result = await pgconn.copy_records_to_table('connection', records=[record], columns=columns)
 				raise aiohttp.web.HTTPFound('/connection')
+		rquery = dict(request.query)
+		if 'xid' in rquery:
+			xid = rquery['xid']
+			config = await pgconn.fetchval(f'select configuration from connection where xid = $1', xid, timeout=4)
+			return aiohttp.web.json_response(config)
 		connections = await pgconn.fetch(f'select {", ".join(columns)} from connection', timeout=4)
 		connections = [dict(x) for x in connections]
 		for x in connections:
-			x['configuration'] = ujson.loads(x['configuration'])
-			if 'password' in x['configuration']:
-				x['configuration']['password'] = '*****'
-		return {'connections': connections}
+			if len(x['configuration']) > 0:
+				logging.info(x['configuration'])
+				x['configuration'] = ujson.loads(x['configuration'])
+				if 'password' in x['configuration']:
+					x['configuration']['password'] = '*****'
+		context = {'connections': connections}
+		resp = aiohttp_jinja2.render_template('html/connection.html', request, context)
+		return resp
 
