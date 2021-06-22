257f54645fa6c6a2ffa8ae7a0c039f2ab1ca04ac
Joel Bondurant <joel@analyticobjects.com>
2021-06-22 08:39:46 -0700

    Query editor wip.

--- query.py
+++ query.py
@@ -19,46 +19,54 @@ def is_valid(form):
 	return True
 
 
-def set_form_defaults(form):
-	logging.debug(f'Raw Form: {form}')
-	form = {k: form[k] for k in FORM_FIELDS}
-	logging.debug(f'Form: {form}')
-	return form
-
-
-@aiohttp_jinja2.template('html/query.html')
 async def query(request):
-	logging.debug(f'Starting query response.')
-	redis = request.app['redis']
 	async with (request.app['pg_pool']).acquire(timeout=2) as pgconn:
-		logging.debug(f'Database connections acquired.')
 		columns = ['xid'] + FORM_FIELDS.copy()
 		if request.method == 'POST':
-			logging.debug(f'Query posted.')
 			form = await request.post()
-			form = set_form_defaults(form)
+			redis = request.app['redis']
 			if is_valid(form):
-				xid = 'x' + secrets.token_hex(16)[1:]
-				record = tuple([xid] + [form[k] for k in FORM_FIELDS])
-				logging.debug(f'Record: {record}')
-				result = await pgconn.copy_records_to_table('query', records=[record], columns=columns)
-				event = {
-					'event_type': 'new',
-					'xid': xid,
-				}
-				redis.publish_json('query', event)
+				if len(form['xid']) == 32:
+					xid = form['xid']
+					record = tuple([xid] + [form[k] for k in FORM_FIELDS])
+					await pgconn.execute('''
+						update query
+						set name = $2, query_text = $3
+						where xid = $1;
+					''', xid, form['name'], form['query_text'])
+					event = {
+						'event_type': 'update',
+						'xid': xid,
+					}
+					redis.publish_json('query', event)
+				else:
+					xid = 'x' + secrets.token_hex(16)[1:]
+					record = tuple([xid] + [form[k] for k in FORM_FIELDS])
+					logging.debug(f'Record: {record}')
+					result = await pgconn.copy_records_to_table('query', records=[record], columns=columns)
+					event = {
+						'event_type': 'new',
+						'xid': xid,
+					}
+					redis.publish_json('query', event)
 				raise aiohttp.web.HTTPFound('/query')
-		logging.debug(f'Fetching queries...')
+		rquery = dict(request.query)
+		if 'xid' in rquery:
+			xid = rquery['xid']
+			query_json = dict(await pgconn.fetchrow(f'''
+				select {", ".join(columns)} from query where xid = $1
+			''', xid, timeout=4))
+			return aiohttp.web.json_response(query_json)
 		queries = await pgconn.fetch(f'select {", ".join(columns)} from query', timeout=4)
 		queries = [dict(x) for x in queries]
-		logging.debug(f'Fetching connections...')
 		cdata = await pgconn.fetch(f'select xid, name from connection', timeout=4)
-		logging.debug(f'Finishing query response.')
 		xconnection_ids = XCONNECTION_IDS.copy() + [x[0] for x in cdata]
 		xconnection_names = XCONNECTION_IDS.copy() + [x[1] + '-' + x[0][:4] for x in cdata]
 		xconnection_labels = [*zip(xconnection_ids, xconnection_names)]
-		return {
+		context = {
 			'queries': queries,
 			'xconnection_labels': xconnection_labels,
 		}
+		resp = aiohttp_jinja2.render_template('html/query.html', request, context)
+		return resp
 
