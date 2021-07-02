00e306dd340bef6a423395de9f532a0f44a4c137
Joel Bondurant <joel@analyticobjects.com>
2021-07-02 10:48:33 -0700

    Dashboard editor wip

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -13,30 +13,29 @@ FORM_FIELDS = [
 ]
 
 
-def is_valid(form):
-	return True
-
-
 async def dashboard(request):
 	user_session, user_xid = login.authenticate(request)
 	async with (request.app['pg_pool']).acquire(timeout=2) as pgconn:
 		if request.method == 'POST':
-			form = await request.post()
-			if is_valid(form):
-				if len(form['xid']) == 32:
-					logging.debug(f'Update dashboard: {record}')
-					await pgconn.execute('''
-						update dashboard
-						set name = $3, configuration = $4, updated = timezone('utc', now())
-						where xid = $1 and user_xid = $2;
-					''', form['xid'], user_xid, form['name'], form['configuration'])
-				else:
-					xid = 'x' + secrets.token_hex(16)[1:]
-					record = tuple([xid, user_xid] + [form[k] for k in FORM_FIELDS])
-					logging.debug(f'New dashboard: {record}')
-					columns = ['xid', 'user_xid'] + FORM_FIELDS.copy()
-					result = await pgconn.copy_records_to_table('dashboard', records=[record], columns=columns)
-				raise aiohttp.web.HTTPFound('/dashboard')
+			event = await request.json()
+			logging.debug(f'Dashboard event posted: {event}')
+			if event['event_type'] ==  'new':
+				xid = 'x' + secrets.token_hex(16)[1:]
+				event['xid'] = xid
+				record = tuple([xid, user_xid] + [event[k] for k in FORM_FIELDS])
+				columns = ['xid', 'user_xid'] + FORM_FIELDS.copy()
+				result = await pgconn.copy_records_to_table('dashboard', records=[record], columns=columns)
+			elif event['event_type'] ==  'update':
+				await pgconn.execute('''
+					update dashboard
+					set name = $3, configuration = $4, updated = timezone('utc', now())
+					where xid = $1 and user_xid = $2;
+				''', event['xid'], user_xid, event['name'], event['configuration'])
+			elif event['event_type'] == 'delete':
+				await pgconn.execute('''
+					delete from dashboard where xid = $1 and user_xid = $2;
+				''', event['xid'], user_xid)
+			return aiohttp.web.json_response({'xid': event['xid']})
 		rquery = dict(request.query)
 		if 'xid' in rquery:
 			xid = rquery['xid']
