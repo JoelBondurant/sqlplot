6702fd010bf397d9daef834ef84fc4df5a3a696d
Joel Bondurant <joel@analyticobjects.com>
2021-07-04 14:01:22 -0700

    Logon/logout from home.

--- routes/home.py
+++ routes/home.py
@@ -2,9 +2,16 @@ import logging
 
 import aiohttp_jinja2
 
+from routes import login
+
 
 async def home(request):
-	logging.debug(f'home hit.')
-	context = {}
+	try:
+		user_session, user_xid = login.authenticate(request)
+		context = {'login': True, 'user_xid': user_xid}
+		logging.debug(f'home login: {user_xid}')
+	except Exception as ex:
+		logging.debug(f'home exception: {ex}')
+		context = {'login': False, 'user_xid': ''}
 	resp = aiohttp_jinja2.render_template('home.html', request, context)
 	return resp
