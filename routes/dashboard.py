8d7b7e46352d894fc45f7a28216380c0250a6749
Joel Bondurant <joelbondurant@protonmail.com>
2021-07-22 07:29:01 -0700

    dashboard auth rewired

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -16,6 +16,7 @@ FORM_FIELDS = [
 
 async def dashboard(request):
 	user_session, user_xid = login.authenticate(request)
+	columns = ['xid'] + FORM_FIELDS.copy()
 	async with (request.app['pg_pool']).acquire(timeout=2) as pgconn:
 		if request.method == 'POST':
 			event = await request.json()
@@ -23,28 +24,28 @@ async def dashboard(request):
 			if event['event_type'] ==  'new':
 				xid = 'x' + secrets.token_hex(16)[1:]
 				event['xid'] = xid
-				record = tuple([xid, user_xid] + [event[k] for k in FORM_FIELDS])
-				columns = ['xid', 'user_xid'] + FORM_FIELDS.copy()
+				record = tuple([xid] + [event[k] for k in FORM_FIELDS])
 				result = await pgconn.copy_records_to_table('dashboard', records=[record], columns=columns)
 				editors = [('editor', txid, 'dashboard', xid) for txid in event['editors']]
-				readers += [('reader', txid, 'dashboard', xid) for txid in event['readers']]
-				auth = editors + readers
-				await pgconn.execute('''
-					delete from "authorization"
-					where object_type = 'dashboard' and object_xid = $1;
-				''', xid)
-				if len(auth) > 0:
-					await pgconn.copy_records_to_table('authorization', records=auth, columns=authorization.COLUMNS)
+				readers = [('reader', txid, 'dashboard', xid) for txid in event['readers']]
+				auth = editors + readers + [('creator', user_xid[:-4]+'0000', 'dashboard', xid)]
+				await pgconn.copy_records_to_table('authorization', records=auth, columns=authorization.COLUMNS)
 			elif event['event_type'] ==  'update':
 				xid = event['xid']
 				await pgconn.execute('''
-					update dashboard
+					update "dashboard" d
 					set name = $3, configuration = $4, updated = timezone('utc', now())
-					where xid = $1 and user_xid = $2;
+					from "authorization" a
+					  on (d.xid = a.object_xid and a.object_type = 'dashboard')
+					join "team_membership" tm
+					  on (a.type in ('creator','editor') and a.team_xid = tm.team_xid)
+					where d.xid = $1 and tm.user_xid = $2;
 				''', xid, user_xid, event['name'], event['configuration'])
 				await pgconn.execute('''
 					delete from "authorization"
-					where object_type = 'dashboard' and object_xid = $1;
+					where "type" in ('editor','reader')
+						and object_type = 'dashboard'
+						and object_xid = $1;
 				''', xid)
 				editors = [('editor', txid, 'dashboard', xid) for txid in event['editors']]
 				readers = [('reader', txid, 'dashboard', xid) for txid in event['readers']]
@@ -54,11 +55,11 @@ async def dashboard(request):
 			elif event['event_type'] == 'delete':
 				xid = event['xid']
 				await pgconn.execute('''
-					delete from authorization
-					where object_type = 'dashboard' and object_xid = $1;
-				''', xid)
-				await pgconn.execute('''
-					delete from dashboard where xid = $1 and user_xid = $2;
+					delete from "dashboard" d
+					using "authorization" a, "team_membership" tm
+					where (d.xid = a.object_xid and a.object_type = 'dashboard')
+					and (a.type in ('creator','editor') and a.team_xid = tm.team_xid)
+					and d.xid = $1 and tm.user_xid = $2;
 				''', xid, user_xid)
 			return aiohttp.web.json_response({'xid': xid})
 		# GET:
@@ -66,14 +67,21 @@ async def dashboard(request):
 		if 'xid' in rquery:
 			xid = rquery['xid']
 			dashboard = dict(await pgconn.fetchrow(f'''
-				select xid, name, configuration
-				from dashboard
-				where xid = $1
-					and user_xid = $2
-				''', xid, user_xid, timeout=4))
+				select distinct {", ".join(['d.' + c for c in columns])}
+				from "dashboard" d
+				left join "authorization" a
+					on (d.xid = a.object_xid and a.object_type = 'dashboard')
+				left join "team_membership" tm
+					on (a.type in ('creator','editor') and a.team_xid = tm.team_xid)
+				where d.xid = $1
+					and (tm.user_xid = $2)
+			''', xid, user_xid, timeout=4))
 			auth = await pgconn.fetch(f'''
-					select type, team_xid from "authorization" where object_type = 'dashboard' and object_xid = $1;
-				''', xid, timeout=4)
+				select type, team_xid
+				from "authorization"
+				where object_type = 'dashboard'
+					and object_xid = $1;
+			''', xid, timeout=4)
 			editors = [x[1] for x in auth if x[0] == 'editor']
 			readers = [x[1] for x in auth if x[0] == 'reader']
 			dashboard['editors'] = editors
@@ -84,8 +92,15 @@ async def dashboard(request):
 			context = {'xid': xidh}
 			return aiohttp_jinja2.render_template('dashboard_view.html', request, context)
 		dashboards = await pgconn.fetch(f'''
-			select xid, name from dashboard where user_xid = $1 order by name
-			''', user_xid, timeout=4)
+			select distinct d.xid, d.name
+			from "dashboard" d
+			left join "authorization" a
+				on (d.xid = a.object_xid and a.object_type = 'dashboard')
+			left join "team_membership" tm
+				on (a.type in ('creator','editor') and a.team_xid = tm.team_xid)
+			where (tm.user_xid = $1)
+			order by 2, 1
+		''', user_xid, timeout=4)
 		dashboards = [dict(x) for x in dashboards]
 		teams = await pgconn.fetch(f'''
 			select distinct t.xid, t.name
@@ -93,8 +108,9 @@ async def dashboard(request):
 			join team t
 				on (tm.team_xid = t.xid)
 			where tm.user_xid = $1
+				and not t.xid = left($1,28)||'0000'
 			order by 2, 1
-			''', user_xid, timeout=4)
+		''', user_xid, timeout=4)
 		teams = [dict(x) for x in teams]
 		context = {
 			'dashboards': dashboards,
