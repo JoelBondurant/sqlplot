2e9736092a5926a5daaae7e74233d3210dd7454f
Joel Bondurant <joelbondurant@protonmail.com>
2021-08-03 23:08:25 -0700

    Payment stub

---
+++ routes/payment.py
@@ -0,0 +1,14 @@
+import logging
+
+import aiohttp
+import aiohttp_jinja2
+
+from routes import login
+
+
+async def payment(request):
+	user_session, user_xid = login.authenticate(request)
+	context = {}
+	resp = aiohttp_jinja2.render_template('payment.html', request, context)
+	return resp
+
